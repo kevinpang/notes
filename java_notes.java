dbc3293d28b6cbfea61c0688f2abb12a4a04ffa8
Kevin Pang <kpanghmc@gmail.com>
2012-03-18 00:02:10 -0700

    Added Java and Node.js note files

---
+++ java_notes.java
@@ -0,0 +1,9 @@
+class java_notes {
+    public static void main(String[] args) {
+        System.out.println("Hello, world");
+        
+        String name = "Kevin";
+        System.out.println(name.length());
+        System.out.printf("Math.PI is %.3f", Math.PI);
+    }
+}
